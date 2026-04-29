b08f27c3f3855447fa4049376179cc9ac77e6180
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-04-29 14:23:09 +0500

    Arithematic Opearations

---
+++ 06-Numpy/03-array-opearations.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Arithematic Opearations
+a1 = np.array([1, 2, 3, 4, 5])
+a2 = np.array([6, 7, 8, 9, 10])
+# a2 = np.array([6, 7, 8, 9, 10, 11]) # If this it will throws an error
+print(a1 + a2)
+print(a1 - a2)
+print(a1 * a2)
+print(a1 / a2)
+print(a1 // a2)
+print(a1 ** a2)
+
