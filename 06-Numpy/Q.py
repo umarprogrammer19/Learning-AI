ec966e4e0fa49c1b152ce0950fbb0abfb2e8161d
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-05-04 18:39:16 +0500

    Questions

---
+++ 06-Numpy/Q.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# Columns: [Age, Math Marks, Science Marks]
+data = np.array(
+    [
+        [18, 85, 78],  # Student 1
+        [19, 92, 88],  # Student 2
+        [17, 76, 95],  # Student 3
+        [18, 65, 70],  # Student 4
+        [20, 90, 85],  # Student 5
+    ]
+)
+
+# 1) Get the shape of the matrix.
+shape_of_matrix = data.shape
+print(shape_of_matrix)
+
+# 2) Find the average age of students.
+average = np.mean(data[:, 0])
+print(average)
