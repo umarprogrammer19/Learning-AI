289713553684b86540ca1ec48dd8d7adafe6de88
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-05-05 17:15:51 +0500

    Creation of Dataframes

--- 07-Pandas/02-dataframes.py
+++ 07-Pandas/02-dataframes.py
@@ -12,3 +12,14 @@ data = {
 df = pd.DataFrame(data)
 print(df)
 
+data_list = [
+    ["Umar", 28, "New York", 65000],
+    ["Raza", 34, "Paris", 70000],
+    ["Asad", 29, "Berlin", 62000],
+    ["Saad", 42, "London", 85000],
+]
+df2 = pd.DataFrame(data_list)
+print(df2)
+columns = ["Name", "Age", "City", "Salary"]
+df2 = pd.DataFrame(data_list, columns)
+print(df2)
