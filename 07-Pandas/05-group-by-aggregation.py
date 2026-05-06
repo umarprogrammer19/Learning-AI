51c0e49ae3062db035ad082bbb872e34d260e97e
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-05-06 12:34:28 +0500

    Group By

--- 07-Pandas/05-group-by-aggregation.py
+++ 07-Pandas/05-group-by-aggregation.py
@@ -27,3 +27,9 @@ print(sum_of_cat)
 # 2) Group by Store and calculate the sum of Sales
 sum_of_store = df.groupby("Store")["Sales"].sum()
 print(sum_of_store)
+
+# Group by multiple columns
+# Group by Category and Store
+
+sums = df.groupby(["Category", "Store"])["Sales"].sum()
+print(sums)
