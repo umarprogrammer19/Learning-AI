74e57d9546596b89a6a4a5ab3b662d898c16093b
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-05-06 12:31:08 +0500

    Sum of sales

--- 07-Pandas/05-group-by-aggregation.py
+++ 07-Pandas/05-group-by-aggregation.py
@@ -7,4 +7,20 @@ data = {
     "Quantity": [10, 15, 12, 18, 8, 20, 15, 25],
     "Date": pd.date_range("2023-01-01", periods=8),
 }
+
 df = pd.DataFrame(data)
+
+#   Category Store  Sales  Quantity        Date
+# 0        A    S1    100        10     2023-01-01
+# 1        B    S1    200        15     2023-01-02
+# 2        A    S2    150        12     2023-01-03
+# 3        B    S2    250        18     2023-01-04
+# 4        A    S1    120         8     2023-01-05
+# 5        B    S2    180        20     2023-01-06
+# 6        A    S2    200        15     2023-01-07
+# 7        B    S1    300        25     2023-01-08
+
+# 1) Group by Category and calculate the sum of Sales
+sum_of_cat = df.groupby("Category")["Sales"].sum()
+print(sum_of_cat)
+
