a02916841a0e5bc9b321b3e94278b5a297b91a19
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-05-06 11:57:51 +0500

    Concatenation

--- 07-Pandas/04-merging-joining-concatenation.py
+++ 07-Pandas/04-merging-joining-concatenation.py
@@ -35,4 +35,28 @@ merge_left = pd.merge(
 merge_right = pd.merge(
     employees, salaries, on="employee_id", how="right"
 )  # Merge Basis Of Salaries
-print(merge_right)
+# print(merge_right)
+
+# Concatination of Two dataframes
+
+df1 = pd.DataFrame(
+    {
+        "A": ["A0", "A1", "A2"],
+        "B": ["B0", "B1", "B2"],
+        "C": ["C0", "C1", "C2"],
+    }
+)
+
+df2 = pd.DataFrame(
+    {
+        "A": ["A3", "A4", "A5"],
+        "B": ["B3", "B4", "B5"],
+        "C": ["C3", "C4", "C5"],
+    }
+)
+
+concat = pd.concat([df1, df2])
+print(concat)
+
+concat_rows = pd.concat([df1, df2], axis=1)
+print(concat_rows)
