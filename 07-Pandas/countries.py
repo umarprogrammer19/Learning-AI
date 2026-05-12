b41cec7d8a8cf4c766890f98d5683b585b18eeaf
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-05-12 13:13:19 +0500

    Pandas Completed

--- 07-Pandas/countries.py
+++ 07-Pandas/countries.py
@@ -27,3 +27,26 @@ regions = df["region"].value_counts().count()
 
 # how many countries lie in Eastern Europe region
 eastern_europe_region = df["region"].value_counts()["Eastern Europe"]
+
+# who is the political leader of the 2nd highest populated country
+pl = df[df["population"] == df["population"].nlargest(2).iloc[1]]["political_leader"]
+
+# how many countries are there whoes political leaders are unknown
+count_unknown = df[df["political_leader"].isna()].value_counts("country").count()
+
+# how many country have Republic in their full name
+count = 0
+
+
+def counting(text):
+    global count
+    if "republic" in text.lower():
+        count += 1
+    return text
+
+
+df["country_long"].apply(counting)
+
+# which country in african region has highest population
+africa_df = df[df["continent"] == "Africa"]
+total = africa_df[africa_df["population"] == africa_df["population"].max()]["country"]
